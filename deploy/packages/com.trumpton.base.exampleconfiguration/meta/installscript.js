c398093439ea20b094c302d2469ca7bdb3f86a4e
Steve Clarke <git@trumpton.uk>
2018-06-20 21:09:56 +0100

    Added copyfile feature for examples

---
+++ deploy/packages/com.trumpton.base.exampleconfiguration/meta/installscript.js
@@ -0,0 +1,11 @@
+
+function Component()
+{
+}
+
+Component.prototype.createOperations = function()
+{
+    component.createOperations();
+    component.addOperation("CopyDirectory", "@TargetDir@/ExampleConfiguration", "@HomeDir@/TrumptonApps") ;
+}
+
