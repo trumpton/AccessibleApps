eb7bb6918b78de4adb8ecc60c28f9b53faff7fe3
Steve Clarke <git@trumpton.uk>
2018-06-22 21:19:07 +0100

    Updated installscript to copyfiles.exe copy examples

--- deploy/packages/com.trumpton.base.exampleconfiguration/meta/installscript.js
+++ deploy/packages/com.trumpton.base.exampleconfiguration/meta/installscript.js
@@ -6,7 +6,11 @@ function Component()
 Component.prototype.createOperations = function()
 {
     component.createOperations();
-    component.addOperation("Mkdir", "@HomeDir@/TrumptonApps") ;
-    component.addOperation("CopyDirectory", "@TargetDir@/ExampleConfiguration/", "@HomeDir@/TrumptonApps/") ;
+    if (systemInfo.productType === "windows") {
+        component.addOperation("Execute", 
+            "@TargetDir@/bin/CopyFiles.exe", 
+            "@TargetDir@/ExampleConfiguration", 
+            "@HomeDir@/TrumptonApps") ;
+    }
 }
 
