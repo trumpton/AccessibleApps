e3589b045357098b392e6dfed33db122af641632
Steve C <steve@trumpton>
2018-06-28 23:31:39 +0100

    Updated pull script

--- pullupdates.sh
+++ pullupdates.sh
@@ -1,6 +1,6 @@
-git pull https://github.com/trumpton/EasyNotepad.git
-git pull https://github.com/trumpton/ContactManager.git
-git pull https://github.com/trumpton/ParseWeb.git
-git pull https://github.com/trumpton/EncryptFolder.git
-git pull https://github.com/trumpton/Lib.git
+(cd EasyNotepad ; git pull)
+(cd ContactManager ; git pull)
+(cd ParseWeb ; git pull)
+(cd EncryptFolder ; git pull)
+(cd Lib ; git pull)
 git pull
