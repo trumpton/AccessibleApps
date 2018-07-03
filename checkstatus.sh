657ccbfae1086c38490e00ac03c2086d14eacacd
Steve Clarke <git@trumpton.uk>
2018-07-03 07:32:41 +0100

    Added check status

---
+++ checkstatus.sh
@@ -0,0 +1,24 @@
+echo "Apps"
+echo "----"
+git status
+echo ""
+echo "ContactManager"
+echo "--------------"
+(cd C0ntactManager ; git status)
+echo ""
+echo "EasyNotepad"
+echo "-----------"
+(cd EasyNotepad ; git status)
+echo ""
+echo "EncryptFolder"
+echo "-------------"
+(cd EncryptFolder ; git status)
+echo ""
+echo "Lib"
+echo "---"
+(cd Lib ; git status)
+echo ""
+echo "ParseWeb"
+echo "--------"
+(cd ParseWeb ; git status)
+echo ""
