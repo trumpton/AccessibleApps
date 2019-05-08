ffa4a36ea7498b0c7a89d368ff9c979f2de884b8
trumpton <git@trumpton.uk>
2019-05-08 13:10:41 +0100

    Added additional git repos

--- downloadapps.sh
+++ downloadapps.sh
@@ -3,3 +3,6 @@ git clone https://github.com/trumpton/ContactManager.git
 git clone https://github.com/trumpton/ParseWeb.git
 git clone https://github.com/trumpton/EncryptFolder.git
 git clone https://github.com/trumpton/Lib.git
+git clone https://github.com/trumpton/EncPad.git
+git clone https://github.com/trumpton/GoogleFetch.git
+git clone https://github.com/trumpton/KeyBackup.git
