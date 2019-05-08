1b3b07a3207e5651b9d76776798b65d1669076a0
Steve Clarke <git@trumpton.uk>
2019-05-08 13:04:52 +0100

    Include new repositories

--- checkstatus.sh
+++ checkstatus.sh
@@ -23,7 +23,16 @@ echo "--------"
 (cd ParseWeb ; git status)
 echo ""
 echo "KeyBackup"
-echo "--------"
+echo "---------"
 (cd KeyBackup ; git status)
 echo ""
+echo "EncPad"
+echo "------"
+(cd EncPad ; git status)
+echo ""
+echo "GoogleFetch"
+echo "-----------"
+(cd GoogleFetch ; git status)
+echo ""
+
 
