05295baed1061ceac2ec833436ee54e84947602b
Steve C <git@trumpton.org.uk>
2019-07-05 15:16:42 +0100

    Included tag update

--- pullupdates.sh
+++ pullupdates.sh
@@ -1,11 +1,11 @@
 mkdir -p EasyNotepad ContactManager ParseWeb EncryptFolder KeyBackup CopyFiles GoogleFetch EncPad
-(cd EasyNotepad ; git pull)
-(cd ContactManager ; git pull)
-(cd ParseWeb ; git pull)
-(cd EncryptFolder ; git pull)
-(cd KeyBackup ; git pull)
-(cd CopyFiles ; git pull)
-(cd GoogleFetch ; git pull)
-(cd EncPad ; git pull)
-(cd Lib ; git pull)
-git pull
+(cd EasyNotepad ; git pull; git fetch --tags)
+(cd ContactManager ; git pull ; git fetch --tags)
+(cd ParseWeb ; git pull ; git fetch --tags)
+(cd EncryptFolder ; git pull ; git fetch --tags)
+(cd KeyBackup ; git pull ; git fetch --tags)
+(cd CopyFiles ; git pull ; git fetch --tags)
+(cd GoogleFetch ; git pull ; git fetch --tags)
+(cd EncPad ; git pull ; git fetch --tags)
+(cd Lib ; git pull ; git fetch --tags)
+git pull ; git fetch --tags
