be9685cc56e12305cec42b64ee0ad2f55299b402
trumpton <git@trumpton.uk>
2019-07-20 08:51:32 +0100

    Added echo guides

--- pullupdates.sh
+++ pullupdates.sh
@@ -1,11 +1,11 @@
 mkdir -p EasyNotepad ContactManager ParseWeb EncryptFolder KeyBackup CopyFiles GoogleFetch EncPad
-(cd EasyNotepad ; git pull ; git fetch --tags )
-(cd ContactManager ; git pull ; git fetch --tags )
-(cd ParseWeb ; git pull ; git fetch --tags )
-(cd EncryptFolder ; git pull ; git fetch --tags )
-(cd KeyBackup ; git pull ; git fetch --tags )
-(cd CopyFiles ; git pull ; git fetch --tags )
-(cd GoogleFetch ; git pull ; git fetch --tags )
-(cd EncPad ; git pull ; git fetch --tags )
-(cd Lib ; git pull ; git fetch --tags )
-git pull ; git fetch --tags
+(cd EasyNotepad ; echo EasyNotepad ; git pull ; git fetch --tags )
+(cd ContactManager ; echo ContactManager ; git pull ; git fetch --tags )
+(cd ParseWeb ; echo ParseWeb ; git pull ; git fetch --tags )
+(cd EncryptFolder ; echo EncryptFolder ; git pull ; git fetch --tags )
+(cd KeyBackup ; echo KeyBackup ; git pull ; git fetch --tags )
+(cd CopyFiles ; echo CopyFiles ; git pull ; git fetch --tags )
+(cd GoogleFetch ; echo GoogleFetch ; git pull ; git fetch --tags )
+(cd EncPad ; echo EncPad ; git pull ; git fetch --tags )
+(cd Lib ; echo Lib ; git pull ; git fetch --tags )
+echo pull ; git pull ; git fetch --tags
