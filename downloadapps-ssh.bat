c99a1dce82d05cd167bae68ad660a94c701ad52b
trumpton <git@trumpton.uk>
2024-10-20 11:11:33 +0100

    Added downloadapps script for windows

---
+++ downloadapps-ssh.bat
@@ -0,0 +1,9 @@
+git clone git@github.com:trumpton/EasyNotepad.git
+git clone git@github.com:trumpton/ContactManager.git
+git clone git@github.com:trumpton/ParseWeb.git
+git clone git@github.com:trumpton/EncryptFolder.git
+git clone git@github.com:trumpton/Lib.git
+git clone git@github.com:trumpton/EncPad.git
+git clone git@github.com:trumpton/GoogleFetch.git
+git clone git@github.com:trumpton/KeyBackup.git
+git clone git@github.com:trumpton/EasyList.git
